20b5d815fc1d417b544700b87a2196736e0d67e4
zouchanggen <zouchanggen@new4g.cn>
2018-02-02 17:00:29 +0800

    add git_revision.cpp file

---
+++ winbuild/git_revision.cpp
@@ -0,0 +1,11 @@
+#include <fc/git_revision.hpp>
+
+#define FC_GIT_REVISION_SHA "0747408f2c21f20272294b17576ab8d051321b02"
+#define FC_GIT_REVISION_UNIX_TIMESTAMP 1505722016
+
+namespace fc { 
+
+const char* const git_revision_sha = FC_GIT_REVISION_SHA;
+const uint32_t git_revision_unix_timestamp = FC_GIT_REVISION_UNIX_TIMESTAMP;
+
+} // end namespace fc
